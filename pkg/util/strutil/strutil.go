e85c88ef7c0711ef4227e736ae3f48b6fb467e17
amazing.gao <amazing.gao@qq.com>
2020-07-23 13:30:11 +0800

    feat(pkg/util/strutil): Init
    support RandString func

---
+++ pkg/util/strutil/strutil.go
@@ -0,0 +1,23 @@
+package strutil
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// RandString return a random n length string
+func RandString(n int) string {
+	b := make([]rune, n)
+
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
