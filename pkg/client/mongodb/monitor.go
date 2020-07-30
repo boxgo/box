b1e4794fd27c9d4bcf437f4732a74d0b9f573641
amazing.gao <amazing.gao@qq.com>
2020-07-30 18:19:49 +0800

    feat(pkg/client/mongodb): Metric changes to global scope.

--- pkg/client/mongodb/monitor.go
+++ pkg/client/mongodb/monitor.go
@@ -2,7 +2,6 @@ package mongodb
 
 import (
 	"context"
-	"sync"
 	"time"
 
 	"github.com/boxgo/box/pkg/config"
@@ -14,14 +13,44 @@ import (
 
 type (
 	Monitor struct {
-		once               sync.Once
-		stopWatch          chan bool
-		cfg                config.SubConfigurator
-		cmdTotal           *prometheus.CounterVec
-		cmdDurationSummary *prometheus.SummaryVec
-		workingSession     *prometheus.GaugeVec
+		stopWatch chan bool
+		cfg       config.SubConfigurator
 	}
 )
+
+var (
+	cmdTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: "",
+			Subsystem: "",
+			Name:      "mongo_client_command_total",
+			Help:      "mongodb client command cmdTotal",
+		},
+		[]string{"command", "error"},
+	)
+	cmdDurationSummary = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace: "",
+			Subsystem: "",
+			Name:      "mongo_client_command_duration_summary",
+			Help:      "mongodb client command duration cmdDurationSummary",
+		},
+		[]string{"command", "error"},
+	)
+	workingSession = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "",
+			Subsystem: "",
+			Name:      "mongo_client_session_in_progress",
+			Help:      "mongo client session in progress gauge",
+		},
+		[]string{},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(cmdDurationSummary, cmdTotal, workingSession)
+}
 
 func newMonitor(cfg config.SubConfigurator) *Monitor {
 	m := &Monitor{
@@ -46,69 +75,27 @@ func (mon *Monitor) PoolEventMonitor() *event.PoolMonitor {
 	}
 }
 
-func (mon *Monitor) init() {
-	mon.once.Do(func() {
-		mon.cmdTotal = prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: "",
-				Subsystem: "",
-				Name:      "mongo_client_command_total",
-				Help:      "mongodb client command cmdTotal",
-			},
-			[]string{"command", "error"},
-		)
-		mon.cmdDurationSummary = prometheus.NewSummaryVec(
-			prometheus.SummaryOpts{
-				Namespace: "",
-				Subsystem: "",
-				Name:      "mongo_client_command_duration_summary",
-				Help:      "mongodb client command duration cmdDurationSummary",
-			},
-			[]string{"command", "error"},
-		)
-		mon.workingSession = prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: "",
-				Subsystem: "",
-				Name:      "mongo_client_session_in_progress",
-				Help:      "mongo client session in progress gauge",
-			},
-			[]string{},
-		)
-
-		prometheus.MustRegister(mon.cmdDurationSummary, mon.cmdTotal, mon.workingSession)
-	})
-}
-
 func (mon *Monitor) started(ctx context.Context, ev *event.CommandStartedEvent) {
-	mon.init()
-
 	logger.Trace(ctx).Debugf("mongo_command_start cmd: %s, reqId: %d, connId: %s, db: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DatabaseName)
 }
 
 func (mon *Monitor) succeeded(ctx context.Context, ev *event.CommandSucceededEvent) {
-	mon.init()
-
 	labels := []string{ev.CommandName, ""}
-	mon.cmdTotal.WithLabelValues(labels...).Inc()
-	mon.cmdDurationSummary.WithLabelValues(labels...).Observe(float64(ev.DurationNanos))
+	cmdTotal.WithLabelValues(labels...).Inc()
+	cmdDurationSummary.WithLabelValues(labels...).Observe(float64(ev.DurationNanos))
 
 	logger.Trace(ctx).Debugf("mongo_command_success cmd: %s, reqId: %d, connId: %s, duration: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, time.Duration(ev.DurationNanos))
 }
 
 func (mon *Monitor) failed(ctx context.Context, ev *event.CommandFailedEvent) {
-	mon.init()
-
 	labels := []string{ev.CommandName, ev.Failure}
-	mon.cmdTotal.WithLabelValues(labels...).Inc()
-	mon.cmdDurationSummary.WithLabelValues(labels...).Observe(float64(ev.DurationNanos))
+	cmdTotal.WithLabelValues(labels...).Inc()
+	cmdDurationSummary.WithLabelValues(labels...).Observe(float64(ev.DurationNanos))
 
 	logger.Trace(ctx).Debugf("mongo_command_error cmd: %s, reqId: %d, connId: %s, duration: %d, error: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DurationNanos, ev.Failure)
 }
 
 func (mon *Monitor) event(ev *event.PoolEvent) {
-	mon.init()
-
 	logger.Debugf("mongo_pool_event type: %s, address: %s, connId: %d, reason: %s", ev.Type, ev.Address, ev.ConnectionID, ev.Reason)
 }
 
@@ -123,7 +110,7 @@ func (mon *Monitor) watch(db *mongo.Client) {
 				close(mon.stopWatch)
 				break
 			default:
-				mon.workingSession.WithLabelValues().Set(float64(db.NumberSessionsInProgress()))
+				workingSession.WithLabelValues().Set(float64(db.NumberSessionsInProgress()))
 			}
 		}
 	}()
