cc09485415f01bfec8d1d717caa73b6ef7d5c22f
amazing.gao <amazing.gao@qq.com>
2020-09-17 15:04:58 +0800

    fix(pkg/client/wukong): Remove `ForceAttemptHTTP2`

--- pkg/client/wukong/transport_1.12.go
+++ pkg/client/wukong/transport_1.12.go
@@ -16,7 +16,6 @@ var (
 			KeepAlive: 30 * time.Second,
 			DualStack: true,
 		}).DialContext,
-		ForceAttemptHTTP2:     true,
 		MaxIdleConns:          100,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
