5cfd83f980cad00aed9a6d8e83be8431221999d4
amazing.gao <amazing.gao@qq.com>
2020-11-07 22:11:12 +0800

    fix(pkg/server/grpcserver/interceptor/zaplog): Fix StreamServerInterceptor

--- pkg/server/grpcserver/interceptor/zaplog/zaplog.go
+++ pkg/server/grpcserver/interceptor/zaplog/zaplog.go
@@ -3,6 +3,7 @@ package zaplog
 import (
 	"context"
 
+	"github.com/boxgo/box/pkg/grpc/wrapper"
 	"github.com/boxgo/box/pkg/logger"
 	"github.com/boxgo/box/pkg/system"
 	"github.com/boxgo/box/pkg/util/strutil"
@@ -33,16 +34,18 @@ func StreamServerInterceptor() grpc.StreamServerInterceptor {
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		ctx := ss.Context()
 		md, _ := metadata.FromIncomingContext(ctx)
-		newCtx := wrapCtx(ctx, md, info.FullMethod)
 
-		logger.TraceRaw(newCtx).Info(">>>")
+		wrapped := wrapper.WrapServerStream(ss)
+		wrapped.WrappedContext = wrapCtx(ctx, md, info.FullMethod)
 
-		err := handler(newCtx, ss)
+		logger.TraceRaw(wrapped.WrappedContext).Info(">>>")
+
+		err := handler(srv, wrapped)
 		if err != nil {
-			logger.TraceRaw(newCtx).Info("xxx", []zap.Field{zap.Any("err", err)}...)
+			logger.TraceRaw(wrapped.WrappedContext).Info("xxx", []zap.Field{zap.Any("err", err)}...)
 		}
 
-		logger.TraceRaw(newCtx).Info("<<<", []zap.Field{zap.Any("resp", "")}...)
+		logger.TraceRaw(wrapped.WrappedContext).Info("<<<")
 
 		return err
 	}
