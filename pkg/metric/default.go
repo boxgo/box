4b019ddf5d7582fa3ee56d80b290234dfe9d4ba1
amazing.gao <amazing.gao@qq.com>
2021-03-03 15:14:54 +0800

    feat(pkg/metric): alias vec

--- pkg/metric/default.go
+++ pkg/metric/default.go
@@ -2,8 +2,6 @@ package metric
 
 import (
 	"net/http"
-
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 // Handler metrics http
@@ -12,21 +10,21 @@ func Handler() http.Handler {
 }
 
 // NewCounterVec creates a new CounterVec based on the provided CounterOpts and partitioned by the given label names.
-func NewCounterVec(name, help string, labels []string) *prometheus.CounterVec {
+func NewCounterVec(name, help string, labels []string) *CounterVec {
 	return Default.NewCounterVec(name, help, labels)
 }
 
 // NewSummaryVec creates a new SummaryVec based on the provided SummaryOpts and partitioned by the given label names.
-func NewSummaryVec(name, help string, labels []string, objectives map[float64]float64) *prometheus.SummaryVec {
+func NewSummaryVec(name, help string, labels []string, objectives map[float64]float64) *SummaryVec {
 	return Default.NewSummaryVec(name, help, labels, objectives)
 }
 
 // NewGaugeVec creates a new GaugeVec based on the provided GaugeOpts and partitioned by the given label names.
-func NewGaugeVec(name, help string, labels []string) *prometheus.GaugeVec {
+func NewGaugeVec(name, help string, labels []string) *GaugeVec {
 	return Default.NewGaugeVec(name, help, labels)
 }
 
 // NewHistogramVec creates a new HistogramVec based on the provided HistogramOpts and partitioned by the given label names.
-func NewHistogramVec(name, help string, labels []string, buckets []float64) *prometheus.HistogramVec {
+func NewHistogramVec(name, help string, labels []string, buckets []float64) *HistogramVec {
 	return Default.NewHistogramVec(name, help, labels, buckets)
 }
