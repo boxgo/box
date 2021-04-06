584596f5367b115766087089b5f7ce3d790d3695
amazing.gao <amazing.gao@qq.com>
2021-04-06 17:45:26 +0800

    feat(pkg/client/mongodb): more alias, eg: ObjectID,Null,Undefined etc

--- pkg/client/mongodb/alias.go
+++ pkg/client/mongodb/alias.go
@@ -2,17 +2,34 @@ package mongodb
 
 import (
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type (
-	A = bson.A
-	D = bson.D
-	E = bson.E
-	M = bson.M
+	A          = primitive.A
+	D          = primitive.D
+	E          = primitive.E
+	M          = primitive.M
+	ObjectID   = primitive.ObjectID
+	Binary     = primitive.Binary
+	DateTime   = primitive.DateTime
+	Decimal128 = primitive.Decimal128
+	JavaScript = primitive.JavaScript
+	MaxKey     = primitive.MaxKey
+	MinKey     = primitive.MinKey
+	Null       = primitive.Null
+	Regex      = primitive.Regex
+	Symbol     = primitive.Symbol
+	Timestamp  = primitive.Timestamp
+	Undefined  = primitive.Undefined
 )
 
 var (
+	ErrDecodeToNil         = bson.ErrDecodeToNil
+	ErrNilContext          = bson.ErrNilContext
+	ErrNilReader           = bson.ErrNilReader
+	ErrNilRegistry         = bson.ErrNilRegistry
 	ErrClientDisconnected  = mongo.ErrClientDisconnected
 	ErrEmptySlice          = mongo.ErrEmptySlice
 	ErrInvalidIndexValue   = mongo.ErrInvalidIndexValue
@@ -24,4 +41,18 @@ var (
 	ErrNonStringIndexName  = mongo.ErrNonStringIndexName
 	ErrUnacknowledgedWrite = mongo.ErrUnacknowledgedWrite
 	ErrWrongClient         = mongo.ErrWrongClient
+	ErrParseNaN            = primitive.ErrParseNaN
+	ErrParseInf            = primitive.ErrParseInf
+	ErrParseNegInf         = primitive.ErrParseNegInf
+)
+
+var (
+	CompareTimestamp          = primitive.CompareTimestamp
+	NewDateTimeFromTime       = primitive.NewDateTimeFromTime
+	NewDecimal128             = primitive.NewDecimal128
+	ParseDecimal128           = primitive.ParseDecimal128
+	ParseDecimal128FromBigInt = primitive.ParseDecimal128FromBigInt
+	NewObjectID               = primitive.NewObjectID
+	NewObjectIDFromTimestamp  = primitive.NewObjectIDFromTimestamp
+	ObjectIDFromHex           = primitive.ObjectIDFromHex
 )
