633b5015feb6a83cb0ec9f950a06d4a9c9810f16
amazing.gao <amazing.gao@qq.com>
2021-04-09 15:47:53 +0800

    feat(pkg/client/mongodb): add options alias

--- pkg/client/mongodb/alias.go
+++ pkg/client/mongodb/alias.go
@@ -4,6 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type (
@@ -23,6 +24,59 @@ type (
 	Symbol     = primitive.Symbol
 	Timestamp  = primitive.Timestamp
 	Undefined  = primitive.Undefined
+)
+
+type (
+	Database                      = mongo.Database
+	Collection                    = mongo.Collection
+	Cursor                        = mongo.Cursor
+	AggregateOptions              = options.AggregateOptions
+	ArrayFilters                  = options.ArrayFilters
+	AutoEncryptionOptions         = options.AutoEncryptionOptions
+	BucketOptions                 = options.BucketOptions
+	BulkWriteOptions              = options.BulkWriteOptions
+	ChangeStreamOptions           = options.ChangeStreamOptions
+	ClientEncryptionOptions       = options.ClientEncryptionOptions
+	ClientOptions                 = options.ClientOptions
+	Collation                     = options.Collation
+	CollectionOptions             = options.CollectionOptions
+	ContextDialer                 = options.ContextDialer
+	CountOptions                  = options.CountOptions
+	CreateCollectionOptions       = options.CreateCollectionOptions
+	CreateIndexesOptions          = options.CreateIndexesOptions
+	CreateViewOptions             = options.CreateViewOptions
+	Credential                    = options.Credential
+	CursorType                    = options.CursorType
+	DataKeyOptions                = options.DataKeyOptions
+	DatabaseOptions               = options.DatabaseOptions
+	DefaultIndexOptions           = options.DefaultIndexOptions
+	DeleteOptions                 = options.DeleteOptions
+	DistinctOptions               = options.DistinctOptions
+	DropIndexesOptions            = options.DropIndexesOptions
+	EncryptOptions                = options.EncryptOptions
+	EstimatedDocumentCountOptions = options.EstimatedDocumentCountOptions
+	FindOneAndDeleteOptions       = options.FindOneAndDeleteOptions
+	FindOneAndReplaceOptions      = options.FindOneAndReplaceOptions
+	FindOneAndUpdateOptions       = options.FindOneAndUpdateOptions
+	FindOneOptions                = options.FindOneOptions
+	FindOptions                   = options.FindOptions
+	FullDocument                  = options.FullDocument
+	GridFSFindOptions             = options.GridFSFindOptions
+	IndexOptions                  = options.IndexOptions
+	InsertManyOptions             = options.InsertManyOptions
+	InsertOneOptions              = options.InsertOneOptions
+	ListCollectionsOptions        = options.ListCollectionsOptions
+	ListDatabasesOptions          = options.ListDatabasesOptions
+	ListIndexesOptions            = options.ListIndexesOptions
+	MarshalError                  = options.MarshalError
+	NameOptions                   = options.NameOptions
+	ReplaceOptions                = options.ReplaceOptions
+	ReturnDocument                = options.ReturnDocument
+	RunCmdOptions                 = options.RunCmdOptions
+	SessionOptions                = options.SessionOptions
+	TransactionOptions            = options.TransactionOptions
+	UpdateOptions                 = options.UpdateOptions
+	UploadOptions                 = options.UploadOptions
 )
 
 var (
