53010e545dfd48d4dda7b4380a707e1d734e80f8
amazing.gao <amazing.gao@qq.com>
2021-04-29 21:58:35 +0800

    fix(pkg/client/mongodb): fix multi instance metric conflict.
    BREAKING CHANGE: change Monitor to interface
    
    fix #4

--- pkg/client/mongodb/metric.go
+++ pkg/client/mongodb/metric.go
@@ -11,8 +11,9 @@ import (
 )
 
 type (
-	Monitor struct {
+	metricMonitor struct {
 		stopWatch chan bool
+		client    *mongo.Client
 	}
 )
 
@@ -39,56 +40,21 @@ var (
 		"mongo client session in progress gauge",
 		[]string{},
 	)
-	defaultMonitor = newMonitor()
 )
 
-func newMonitor() *Monitor {
-	m := &Monitor{
+func newMonitor() Monitor {
+	m := &metricMonitor{
 		stopWatch: make(chan bool),
 	}
 
 	return m
 }
 
-func (mon *Monitor) CommandMonitor() *event.CommandMonitor {
-	return &event.CommandMonitor{
-		Started:   mon.started,
-		Succeeded: mon.succeeded,
-		Failed:    mon.failed,
-	}
+func (mon *metricMonitor) Setup(client *mongo.Client) {
+	mon.client = client
 }
 
-func (mon *Monitor) PoolEventMonitor() *event.PoolMonitor {
-	return &event.PoolMonitor{
-		Event: mon.event,
-	}
-}
-
-func (mon *Monitor) started(ctx context.Context, ev *event.CommandStartedEvent) {
-	logger.Trace(ctx).Debugf("mongo_command_start cmd: %s, reqId: %d, connId: %s, db: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DatabaseName)
-}
-
-func (mon *Monitor) succeeded(ctx context.Context, ev *event.CommandSucceededEvent) {
-	labels := []string{ev.CommandName, ""}
-	cmdTotal.WithLabelValues(labels...).Inc()
-	cmdDuration.WithLabelValues(labels...).Observe(time.Duration(ev.DurationNanos).Seconds())
-
-	logger.Trace(ctx).Debugf("mongo_command_success cmd: %s, reqId: %d, connId: %s, duration: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, time.Duration(ev.DurationNanos))
-}
-
-func (mon *Monitor) failed(ctx context.Context, ev *event.CommandFailedEvent) {
-	labels := []string{ev.CommandName, ev.Failure}
-	cmdTotal.WithLabelValues(labels...).Inc()
-	cmdDuration.WithLabelValues(labels...).Observe(time.Duration(ev.DurationNanos).Seconds())
-
-	logger.Trace(ctx).Debugf("mongo_command_error cmd: %s, reqId: %d, connId: %s, duration: %d, error: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DurationNanos, ev.Failure)
-}
-
-func (mon *Monitor) event(ev *event.PoolEvent) {
-	logger.Debugf("mongo_pool_event type: %s, address: %s, connId: %d, reason: %s", ev.Type, ev.Address, ev.ConnectionID, ev.Reason)
-}
-
-func (mon *Monitor) watch(db *mongo.Client) {
+func (mon *metricMonitor) Serve() {
 	go func() {
 		for {
 			time.Sleep(time.Second)
@@ -99,12 +65,36 @@ func (mon *Monitor) watch(db *mongo.Client) {
 				close(mon.stopWatch)
 				break
 			default:
-				workingSession.WithLabelValues().Set(float64(db.NumberSessionsInProgress()))
+				workingSession.WithLabelValues().Set(float64(mon.client.NumberSessionsInProgress()))
 			}
 		}
 	}()
 }
 
-func (mon *Monitor) shutdown() {
+func (mon *metricMonitor) Shutdown() {
 	mon.stopWatch <- true
+}
+
+func (mon *metricMonitor) Started(ctx context.Context, ev *event.CommandStartedEvent) {
+	logger.Trace(ctx).Debugf("mongo_command_start cmd: %s, reqId: %d, connId: %s, db: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DatabaseName)
+}
+
+func (mon *metricMonitor) Succeeded(ctx context.Context, ev *event.CommandSucceededEvent) {
+	labels := []string{ev.CommandName, ""}
+	cmdTotal.WithLabelValues(labels...).Inc()
+	cmdDuration.WithLabelValues(labels...).Observe(time.Duration(ev.DurationNanos).Seconds())
+
+	logger.Trace(ctx).Debugf("mongo_command_success cmd: %s, reqId: %d, connId: %s, duration: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, time.Duration(ev.DurationNanos))
+}
+
+func (mon *metricMonitor) Failed(ctx context.Context, ev *event.CommandFailedEvent) {
+	labels := []string{ev.CommandName, ev.Failure}
+	cmdTotal.WithLabelValues(labels...).Inc()
+	cmdDuration.WithLabelValues(labels...).Observe(time.Duration(ev.DurationNanos).Seconds())
+
+	logger.Trace(ctx).Debugf("mongo_command_error cmd: %s, reqId: %d, connId: %s, duration: %d, error: %s", ev.CommandName, ev.RequestID, ev.ConnectionID, ev.DurationNanos, ev.Failure)
+}
+
+func (mon *metricMonitor) Event(ev *event.PoolEvent) {
+	logger.Debugf("mongo_pool_event type: %s, address: %s, connId: %d, reason: %s", ev.Type, ev.Address, ev.ConnectionID, ev.Reason)
 }
